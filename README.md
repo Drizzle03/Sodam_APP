0849b6e51bb3d10ba730dd76747e043ba3d4ccd0
sgwanlee <seonggwan.lee@gmail.com>
2024-12-21 21:38:50 +0900

    Change to accept API key input

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# 환경변수 설정
-
-.env.local 파일 생성 후, `EXPO_PUBLIC_OPENAI_API_KEY` 변수에 OpenAI API Key를 입력해주세요.
-
 # 웹뷰
 
 app/(app)/home.tsx 파일에서 웹뷰 코드를 확인해주세요.
--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,11 +1,29 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, TextInput, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import WebView from "react-native-webview";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Home = () => {
+  const [apiKey, setApiKey] = useState<string>();
   const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    AsyncStorage.getItem("apiKey").then((value) => {
+      if (value) {
+        setApiKey(value);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (apiKey) {
+      AsyncStorage.setItem("apiKey", apiKey);
+    }
+  }, [apiKey]);
+
   return (
     <View className="flex-1 bg-primary">
       <StatusBar style="auto" />
@@ -14,16 +32,31 @@ const Home = () => {
         className="flex-1"
         style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
       >
+        {!apiKey && (
+          <View className="flex-1 justify-center items-center ">
+            <Text className="text-primary font-bold">
+              Enter your openai api key
+            </Text>
+            <TextInput
+              value={apiKey}
+              onChangeText={setApiKey}
+              placeholder="Enter your openai api key"
+              className="text-primary border h-10"
+            />
+          </View>
+        )}
         {/* 웹뷰 */}
-        <WebView
-          key={1}
-          source={{
-            uri: `https://chat-sodam.vercel.app/?apiKey=${process.env.EXPO_PUBLIC_OPENAI_API_KEY}`,
-          }}
-          allowsInlineMediaPlayback={true}
-          mediaPlaybackRequiresUserAction={false}
-          originWhitelist={["*"]}
-        />
+        {apiKey && (
+          <WebView
+            key={1}
+            source={{
+              uri: `https://chat-sodam.vercel.app/?apiKey=${apiKey}`,
+            }}
+            allowsInlineMediaPlayback={true}
+            mediaPlaybackRequiresUserAction={false}
+            originWhitelist={["*"]}
+          />
+        )}
       </View>
     </View>
   );
